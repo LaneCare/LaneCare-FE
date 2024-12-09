45a2d59b02f964b5f02321612672530be1912aee
RadityaDito <raditya.ihsan@ui.ac.id>
2024-12-10 01:46:54 +0700

    middleware rusak

--- middleware.ts
+++ middleware.ts
@@ -11,11 +11,11 @@ export default withAuth(
     console.log("Ini Middleware");
     console.log(request.nextauth.token);
     // If the route is not public and the user is not authenticated, redirect to /login
-    if (!isPublicRoute && !request.nextauth.token) {
-      console.log("Redirecting to /login");
-      const newUrl = new URL("/login", request.nextUrl.origin);
-      return Response.redirect(newUrl);
-    }
+    // if (!isPublicRoute && !request.nextauth.token) {
+    //   console.log("Redirecting to /login");
+    //   const newUrl = new URL("/login", request.nextUrl.origin);
+    //   return Response.redirect(newUrl);
+    // }
   },
   {
     callbacks: {
